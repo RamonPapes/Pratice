05c7f06fcc588c1f7aaf4bf07a3317f13db3c310
RamonPapes <ramonpapes@gmail.com>
2022-03-24 09:39:58 -0300

    ...

--- Code_Python/bublleSort.py
+++ Code_Python/bublleSort.py
@@ -7,7 +7,21 @@ class Ordenador:
                 if lista[j] > lista[j+1]:
                     lista[j],lista[j+1] = lista[j+1],lista[j]
 
+    def bolha_melhorado(self,lista):
+        fim = len(lista)
+
+        for i in range(fim-1, 0, -1):
+            trocou = False
+            for j in range(i):
+                if lista[j] > lista[j+1]:
+                    lista[j],lista[j+1] = lista[j+1],lista[j]
+                    trocou = True
+            if trocou == False :
+                return 
+
+
                
+          
 
 
 o = Ordenador()
